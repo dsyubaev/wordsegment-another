1d4f0edd73d175707118bbaf676fa6f4dad53fd8
dmitry syubaev <subaevdi@gmail.com>
2023-07-10 17:20:43 +0300

    added tests and data files (#1)

---
+++ tests/test_segmentator.rs
@@ -0,0 +1,218 @@
+mod common;
+
+fn join(result: Vec<&str>) -> String {
+    result
+        .into_iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>()
+        .join("")
+}
+
+#[test]
+fn test_segment_0() {
+    // using common code.
+    let seg = common::segmentator();
+    let result = vec!["choose", "spain"];
+
+    assert_eq!(seg.segment(join(result.clone()).as_str()), result);
+}
+
+#[test]
+fn test_segment_1() {
+    // using common code.
+    let seg = common::segmentator();
+    let result = vec!["this", "is", "a", "test"];
+
+    assert_eq!(seg.segment(join(result.clone()).as_str()), result);
+}
+
+#[test]
+fn test_segment_2() {
+    // using common code.
+    let seg = common::segmentator();
+    let result = vec![
+        "when",
+        "in",
+        "the",
+        "course",
+        "of",
+        "human",
+        "events",
+        "it",
+        "becomes",
+        "necessary",
+    ];
+
+    assert_eq!(seg.segment(join(result.clone()).as_str()), result);
+}
+
+#[test]
+fn test_segment_3() {
+    // using common code.
+    let seg = common::segmentator();
+    let result = vec!["who", "represents"];
+
+    assert_eq!(seg.segment(join(result.clone()).as_str()), result);
+}
+
+#[test]
+fn test_segment_4() {
+    // using common code.
+    let seg = common::segmentator();
+    let result = vec!["experts", "exchange"];
+
+    assert_eq!(seg.segment(join(result.clone()).as_str()), result);
+}
+
+#[test]
+fn test_segment_5() {
+    // using common code.
+    let seg = common::segmentator();
+    let result = vec!["speed", "of", "art"];
+
+    assert_eq!(seg.segment(join(result.clone()).as_str()), result);
+}
+
+#[test]
+fn test_segment_6() {
+    // using common code.
+    let seg = common::segmentator();
+    let result = vec!["now", "is", "the", "time", "for", "all", "good"];
+
+    assert_eq!(seg.segment(join(result.clone()).as_str()), result);
+}
+
+#[test]
+fn test_segment_7() {
+    // using common code.
+    let seg = common::segmentator();
+    let result = vec!["it", "is", "a", "truth", "universally", "acknowledged"];
+
+    assert_eq!(seg.segment(join(result.clone()).as_str()), result);
+}
+
+#[test]
+fn test_segment_8() {
+    // using common code.
+    let seg = common::segmentator();
+    let result = vec![
+        "it", "was", "a", "bright", "cold", "day", "in", "april", "and", "the", "clocks", "were",
+        "striking", "thirteen",
+    ];
+
+    assert_eq!(seg.segment(join(result.clone()).as_str()), result);
+}
+
+#[test]
+fn test_segment_9() {
+    // using common code.
+    let seg = common::segmentator();
+    let result = vec![
+        "it",
+        "was",
+        "the",
+        "best",
+        "of",
+        "times",
+        "it",
+        "was",
+        "the",
+        "worst",
+        "of",
+        "times",
+        "it",
+        "was",
+        "the",
+        "age",
+        "of",
+        "wisdom",
+        "it",
+        "was",
+        "the",
+        "age",
+        "of",
+        "foolishness",
+    ];
+
+    assert_eq!(seg.segment(join(result.clone()).as_str()), result);
+}
+
+#[test]
+fn test_segment_10() {
+    // using common code.
+    let seg = common::segmentator();
+    let result = vec![
+        "as",
+        "gregor",
+        "samsa",
+        "awoke",
+        "one",
+        "morning",
+        "from",
+        "uneasy",
+        "dreams",
+        "he",
+        "found",
+        "himself",
+        "transformed",
+        "in",
+        "his",
+        "bed",
+        "into",
+        "a",
+        "gigantic",
+        "insect",
+    ];
+
+    assert_eq!(seg.segment(join(result.clone()).as_str()), result);
+}
+
+#[test]
+fn test_segment_11() {
+    // using common code.
+    let seg = common::segmentator();
+    let result = vec![
+        "in", "a", "hole", "in", "the", "ground", "there", "lived", "a", "hobbit", "not", "a",
+        "nasty", "dirty", "wet", "hole", "filled", "with", "the", "ends", "of", "worms", "and",
+        "an", "oozy", "smell", "nor", "yet", "a", "dry", "bare", "sandy", "hole", "with",
+        "nothing", "in", "it", "to", "sit", "down", "on", "or", "to", "eat", "it", "was", "a",
+        "hobbit", "hole", "and", "that", "means", "comfort",
+    ];
+
+    assert_eq!(seg.segment(join(result.clone()).as_str()), result);
+}
+
+#[test]
+fn test_segment_12() {
+    // using common code.
+    let seg = common::segmentator();
+    let result = vec![
+        "far",
+        "out",
+        "in",
+        "the",
+        "uncharted",
+        "backwaters",
+        "of",
+        "the",
+        "unfashionable",
+        "end",
+        "of",
+        "the",
+        "western",
+        "spiral",
+        "arm",
+        "of",
+        "the",
+        "galaxy",
+        "lies",
+        "a",
+        "small",
+        "un",
+        "regarded",
+        "yellow",
+        "sun",
+    ];
+
+    assert_eq!(seg.segment(join(result.clone()).as_str()), result);
+}
